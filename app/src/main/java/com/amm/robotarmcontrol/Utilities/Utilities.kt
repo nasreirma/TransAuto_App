0bd39c0f897273a567507bbea4099e5f6e25d2a2
Nasrellah Irhmar <nasreirma@gmail.com>
2021-05-16 20:25:28 +0100

    no need to check if biometrics is availbale or not

--- app/src/main/java/com/amm/robotarmcontrol/Utilities/Utilities.kt
+++ app/src/main/java/com/amm/robotarmcontrol/Utilities/Utilities.kt
@@ -29,25 +29,4 @@ object Utilities {
             e.printStackTrace()
         }
     }
-
-
-    fun isBiometricHardWareAvailable(con: Context):Boolean {
-        var result=false
-        val biometricManager = BiometricManager.from(con)
-        when (biometricManager.canAuthenticate()) {
-            BiometricManager.BIOMETRIC_SUCCESS ->result=true
-            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE ->result=false
-            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> result=false
-            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED ->result=false
-        }
-        return result
-    }
-
-
-    fun deviceHasPasswordPinLock(con: Context):Boolean {
-        val keymgr=con.getSystemService(AppCompatActivity.KEYGUARD_SERVICE) as KeyguardManager
-        if(keymgr.isKeyguardSecure)
-            return true
-        return false
-    }
 }
